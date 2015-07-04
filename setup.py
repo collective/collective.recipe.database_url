a3e61aadb7a3dde419b67437e8799dbb975b0d74
Alex Clark <aclark@aclark.net>
2015-07-03 20:06:06 -0400

    Add files

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages
+from setuptools import setup
+
+VERSION='0.0.1'
+
+setup(
+    author='Alex Clark',
+    author_email='aclark@aclark.net',
+    classifiers=[],
+    description='',
+    entry_points={
+    },
+    keywords='',
+    include_package_data=True,
+    install_requires=[
+        'dj-database-url',
+    ],
+    long_description=open('README.rst').read() + '\n' + open('CHANGES.rst').read(),
+    name='',
+    namespace_packages=[
+    ],
+    packages=find_packages(),
+    url='',
+    version=VERSION,
+    zip_safe=False,
+)
